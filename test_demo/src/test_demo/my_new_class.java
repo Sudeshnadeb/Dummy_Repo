6142ac9fbbd5df175f81c702ca3aa06261c1335d
Jayanta Deb <jayanta_debx@yahoo.com>
2019-06-28 20:30:32 +0100

    my class save....

---
+++ test_demo/src/test_demo/my_new_class.java
@@ -0,0 +1,10 @@
+package test_demo;
+
+public class my_new_class {
+
+	public static void main(String args[]) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello Word...");
+	}
+
+}
